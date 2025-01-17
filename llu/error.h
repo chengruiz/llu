7858109aa8bcc2a449327a559b5d4bb669799a29
Chengrui Zhu <jewel@zju.edu.cn>
2025-01-17 15:39:06 +0800

    add macro LLU_ASSERT_EQ

--- llu/error.h
+++ llu/error.h
@@ -9,6 +9,7 @@
 #define LLU_ERROR(...)             throw std::runtime_error(fmt::format(LLU_FILELINE() " " __VA_ARGS__))
 #define LLU_ASSERT_NOT(cond, ...)  if (cond) LLU_ERROR(__VA_ARGS__)
 #define LLU_ASSERT(cond, ...)      LLU_ASSERT_NOT(not (cond), __VA_ARGS__)
+#define LLU_ASSERT_EQ(a, b, ...)   LLU_ASSERT(a == b, __VA_ARGS__)
 
 #define LLU_PRINT(...)             fmt::print("{}\n"    , fmt::format(__VA_ARGS__));
 #define LLU_INFO(...)              fmt::print("{}{}{}\n", ::llu::kGreen , fmt::format(__VA_ARGS__), ::llu::kClear);
