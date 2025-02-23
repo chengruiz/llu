9ab737080db3dac9675d35362d82d1e6dbd51d08
Chengrui Zhu <jewel@zju.edu.cn>
2025-02-23 10:38:25 +0800

    add environment variable retrieval functions and tests

---
+++ llu/env.h
@@ -0,0 +1,29 @@
+#ifndef LLU_ENV_H_
+#define LLU_ENV_H_
+
+#include <cstdlib>
+#include <string>
+
+#include <llu/error.h>
+
+namespace llu {
+inline bool getenv(const char *name, std::string &result) {
+  const char *var = std::getenv(name);
+  if (var == nullptr) return false;
+  result = var;
+  return true;
+}
+
+inline bool getenv(const char *name, long &result) {
+  const char *var = std::getenv(name);
+  if (var == nullptr) return false;
+  char *end{nullptr};
+  long value = std::strtol(var, &end, 10);
+  LLU_ASSERT(*end == '\0', "Environment Variable `{}` ({}) cannot be converted to a integer.", name, var);
+
+  result = value;
+  return true;
+}
+} // namespace llu
+
+#endif // LLU_ENV_H_
---
+++ llu_tests/env_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include <llu/env.h>
+
+TEST(LLU_ENV_TEST, LLU_ENV_TEST) {
+  std::string str_val;
+  long int_val;
+
+  setenv("A_VAR", "123", 1);
+
+  ASSERT_FALSE(llu::getenv("NOT_A_VAR", str_val)) << "Expected not a variable";
+  ASSERT_FALSE(llu::getenv("NOT_A_VAR", int_val)) << "Expected not a variable";
+
+  ASSERT_TRUE(llu::getenv("HOME", str_val)) << "Expected a variable";
+  ASSERT_TRUE(llu::getenv("A_VAR", int_val)) << "Expected a variable";
+  ASSERT_EQ(int_val, 123) << "Expected 123";
+
+  try {
+    llu::getenv("HOME", int_val);
+    ASSERT_TRUE(false) << "Expected an exception";
+  } catch (const std::runtime_error &e) {}
+}
