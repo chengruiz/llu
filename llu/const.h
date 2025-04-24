f8d8297fd4fed07f7dae7414104e0d5f3777cca1
Chengrui Zhu <jewel@zju.edu.cn>
2025-04-24 11:01:56 +0800

    feat: contain more messages in LLU_ASSERT and LLU_ASSERT_EQ

--- llu/const.h
+++ llu/const.h
@@ -2,13 +2,17 @@
 #define LLU_CONST_H_
 
 namespace llu {
-constexpr const char *kClear   = "\033[0m";
-constexpr const char *kRed     = "\033[1;31m";
-constexpr const char *kGreen   = "\033[1;32m";
-constexpr const char *kYellow  = "\033[1;33m";
-constexpr const char *kBlue    = "\033[1;34m";
-constexpr const char *kMagenta = "\033[1;35m";
-constexpr const char *kCyan    = "\033[1;36m";
+constexpr const char *kClear     = "\033[0m";
+constexpr const char *kBold      = "\033[1m";
+constexpr const char *kDim       = "\033[2m";
+constexpr const char *kItalic    = "\033[3m";
+constexpr const char *kUnderline = "\033[4m";
+constexpr const char *kRed       = "\033[1;31m";
+constexpr const char *kGreen     = "\033[1;32m";
+constexpr const char *kYellow    = "\033[1;33m";
+constexpr const char *kBlue      = "\033[1;34m";
+constexpr const char *kMagenta   = "\033[1;35m";
+constexpr const char *kCyan      = "\033[1;36m";
 
 constexpr double kEPS = 1e-8;
 }  // namespace llu
--- llu/error.h
+++ llu/error.h
@@ -9,15 +9,40 @@
 #include <llu/const.h>
 #include <llu/macro.h>
 
-#define LLU_ERROR(...) throw std::runtime_error(fmt::format(LLU_FILELINE() " " __VA_ARGS__))
 #define LLU_PRINT(...) fmt::print("{}\n", fmt::format(__VA_ARGS__));
 #define LLU_INFO(...)  fmt::print("{}{}{}\n", ::llu::kGreen, fmt::format(__VA_ARGS__), ::llu::kClear);
 #define LLU_WARN(...)  fmt::print("{}{}{}\n", ::llu::kYellow, fmt::format(__VA_ARGS__), ::llu::kClear);
 #define LLU_CRIT(...)  fmt::print("{}{}{}\n", ::llu::kRed, fmt::format(__VA_ARGS__), ::llu::kClear);
 
-#define LLU_ASSERT_NOT(cond, ...) \
-  if (cond) LLU_ERROR(__VA_ARGS__)
-#define LLU_ASSERT(cond, ...)    LLU_ASSERT_NOT(not(cond), __VA_ARGS__)
-#define LLU_ASSERT_EQ(a, b, ...) LLU_ASSERT((a) == (b), __VA_ARGS__)
+namespace llu {
+namespace impl {
+template <typename... Args>
+[[noreturn]] void errorHelper(const char *file_line, const char *msg, Args &&...args) {
+  std::string prefix = fmt::format("{} ", file_line);
+  throw std::runtime_error(prefix + fmt::format(msg, std::forward<Args>(args)...));
+}
 
+template <typename T, typename... Args>
+void assertHelper(const T &condition, const char *condition_str, const char *file_line, const char *msg,
+                  Args &&...args) {
+  if (condition) return;
+  std::string prefix = fmt::format("{} Assertion {}{}{} failed: ", file_line, kUnderline, condition_str, kClear);
+  throw std::runtime_error(prefix + fmt::format(msg, std::forward<Args>(args)...));
+}
+
+template <typename T1, typename T2, typename... Args>
+void assertEqHelper(T1 value, T2 expected, const char *value_str, const char *expected_str, const char *file_line,
+                    const char *msg, Args &&...args) {
+  if (value == expected) return;
+  std::string prefix = fmt::format("{} Assertion {}{} == {}{} (i.e. {} == {}) failed: ", file_line, kUnderline,
+                                   value_str, expected_str, kClear, value, expected);
+  throw std::runtime_error(prefix + fmt::format(msg, std::forward<Args>(args)...));
+}
+}  // namespace impl
+}  // namespace llu
+
+#define LLU_ERROR(...)             ::llu::impl::errorHelper(LLU_FILELINE(), __VA_ARGS__)
+#define LLU_ASSERT(condition, ...) ::llu::impl::assertHelper(condition, #condition, LLU_FILELINE(), __VA_ARGS__)
+#define LLU_ASSERT_EQ(value, expected, ...) \
+  ::llu::impl::assertEqHelper(value, expected, #value, #expected, LLU_FILELINE(), __VA_ARGS__)
 #endif  //  LLU_ERROR_H_
