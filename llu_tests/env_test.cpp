7006153b648d992208a5a10ce067a2f0a0f4040d
Chengrui Zhu <jewel@zju.edu.cn>
2025-11-26 20:15:30 +0800

    test: enhance getenv tests for boolean values

--- llu_tests/env_test.cpp
+++ llu_tests/env_test.cpp
@@ -20,6 +20,42 @@ TEST(LLU_ENV_TEST, LLU_ENV_TEST) {
     ASSERT_TRUE(false) << "Expected an exception";
   } catch (const std::exception &e) {}
 
+  bool bool_val;
+  ASSERT_FALSE(llu::getenv("NOT_A_VAR", bool_val)) << "Expected not a variable";
+
+  setenv("TRUE1", "1", 1);
+  setenv("TRUE2", "true", 1);
+  setenv("TRUE3", "yes", 1);
+  setenv("TRUE4", "on", 1);
+  setenv("FALSE1", "0", 1);
+  setenv("FALSE2", "false", 1);
+  setenv("FALSE3", "no", 1);
+  setenv("FALSE4", "off", 1);
+
+  ASSERT_TRUE(llu::getenv("TRUE1", bool_val)) << "Expected TRUE1 to be set";
+  ASSERT_TRUE(bool_val);
+  ASSERT_TRUE(llu::getenv("TRUE2", bool_val)) << "Expected TRUE2 to be set";
+  ASSERT_TRUE(bool_val);
+  ASSERT_TRUE(llu::getenv("TRUE3", bool_val)) << "Expected TRUE3 to be set";
+  ASSERT_TRUE(bool_val);
+  ASSERT_TRUE(llu::getenv("TRUE4", bool_val)) << "Expected TRUE4 to be set";
+  ASSERT_TRUE(bool_val);
+
+  ASSERT_TRUE(llu::getenv("FALSE1", bool_val)) << "Expected FALSE1 to be set";
+  ASSERT_FALSE(bool_val);
+  ASSERT_TRUE(llu::getenv("FALSE2", bool_val)) << "Expected FALSE2 to be set";
+  ASSERT_FALSE(bool_val);
+  ASSERT_TRUE(llu::getenv("FALSE3", bool_val)) << "Expected FALSE3 to be set";
+  ASSERT_FALSE(bool_val);
+  ASSERT_TRUE(llu::getenv("FALSE4", bool_val)) << "Expected FALSE4 to be set";
+  ASSERT_FALSE(bool_val);
+
+  setenv("BAD_BOOL", "maybe", 1);
+  try {
+    llu::getenv("BAD_BOOL", bool_val);
+    ASSERT_TRUE(false) << "Expected an exception";
+  } catch (const std::exception &e) {}
+
   setenv("MY_PATHS1", ":/a:/b::/c:", 1);
   setenv("MY_PATHS2", "", 1);
   setenv("MY_PATHS3", ":::::", 1);
